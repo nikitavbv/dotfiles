55d35b591aff0de124a9bb6cd7879170f520de26
Nikita Volobuev <nikitavbv@gmail.com>
2021-08-17 21:41:22 +0300

    fix(install): fix font installation

--- install.sh
+++ install.sh
@@ -1,2 +1,1 @@
-curl -O "~/Library/Fonts/Roboto Mono for Powerline.ttf" https://github.com/powerline/fonts/blob/master/RobotoMono/Roboto%20Mono%20for%2
-0Powerline.ttf\?raw\=true
+curl -L "https://github.com/powerline/fonts/raw/master/RobotoMono/Roboto%20Mono%20for%20Powerline.ttf" --output "$HOME/Library/Fonts/Roboto Mono for Powerline.ttf"
